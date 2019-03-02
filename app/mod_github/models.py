db05eb57fe9f913ee5861d6b421f3a03537a02dd
Jake <jakemassoth16@gmail.com>
2019-03-02 18:13:28 +0100

    create projects db model

--- app/mod_github/models.py
+++ app/mod_github/models.py
@@ -1,2 +1,13 @@
-class Projects:
-    pass
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from main import db
+
+
+class Projects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.Text, unique=True, nullable=True)
+    last_commit = db.Column(db.DateTime, unique=False, nullable=False)
+    stars = db.Column(db.Integer, unique=False, nullable=False)
+    forks = db.Column(db.Integer, unique=False, nullable=False)
+    language = db.Column(db.String(20), unique=False, nullable=False)
