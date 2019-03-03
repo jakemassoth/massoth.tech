4e9b14bbf92db5f0ac81c43d879a6fb12f2b8603
Jake <jakemassoth16@gmail.com>
2019-03-03 20:46:05 +0100

    update landing controllers to play nice with the api

--- app/mod_landing/controllers.py
+++ app/mod_landing/controllers.py
@@ -2,14 +2,15 @@
 import json
 from flask import render_template
 from . import mod_landing
-
-
-filename = 'response.json'
-
-with open(filename, 'r') as f:
-    projects = json.load(f)
+import requests
+# needed to get requests to run on GAE
+from requests_toolbelt.adapters import appengine
 
 
 @mod_landing.route('/')
 def index():
-    return render_template('landing/landing.html', projects=projects)
+    # MAKE SURE TO CHANGE BEFORE PRODUCTION!!!!!!!!!!!!
+    projects = requests.get("http://localhost:8080/updater/")
+    projects = projects.json()
+    print projects['projects']
+    return render_template('landing/landing.html', projects=projects['projects'])
