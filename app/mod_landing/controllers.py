113149b23ff1b0ea4cf50e9f8767003ceb1d9d0f
jake <jakemassoth16@gmail.com>
2020-01-05 13:28:03 +0300

    added github api error handling

--- app/mod_landing/controllers.py
+++ app/mod_landing/controllers.py
@@ -5,12 +5,12 @@ import requests
 # needed to get requests to run on GAE
 from requests_toolbelt.adapters import appengine
 from . import mod_landing
-# TODO: make the last commit more pretty
 
 
 @mod_landing.route('/')
 def index():
     projects = requests.get("https://api.github.com/users/bluzomby/repos")
-    projects = projects.json()
-    projects = sorted(projects, key=lambda x: x['updated_at'], reverse=True)
+    if projects.ok:
+        projects = projects.json()
+        projects = sorted(projects, key=lambda x: x['updated_at'], reverse=True)
     return render_template('landing/landing.html', projects=projects)
