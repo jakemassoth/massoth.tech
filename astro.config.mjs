421ed6b606cf5fdd8f5916d52c6140d1f2ce5f21
Jake Massoth <jakemassoth@storyteq.com>
2024-10-26 18:25:43 +0200

    nvm github pages doesn't really do what I want

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jakemassoth.github.io",
+  output: "server",
+  adapter: vercel(),
 });
